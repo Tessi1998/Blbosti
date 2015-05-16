b1fd43562612a23ac6d8d0e8eb265863f75bfeb5
Tessi1998 <lubijova.terka@gmail.com>
2015-05-16 22:48:19 +0200

    Update Narcisova-prace.js

--- Narcisova-prace.js
+++ Narcisova-prace.js
@@ -3,7 +3,7 @@ API.on(API.CHAT, tessijesuper);
 
 function tessijesuper(love){
 	var msgData = love;
-	if(msgData.message.indexOf("!maluj") > -1){
+	if(msgData.message.indexOf("!paint") > -1){
 		console.log(msgData.un);
 		if(msgData.un == "Tessi Tess" || msgData.un == "Narcis"){
 			var narcisekjekrasavec = msgData.message.replace(" !paint", "");
