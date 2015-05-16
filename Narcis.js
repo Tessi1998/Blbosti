740b02422b3845a9dfbeafd771348a58bdc5fce7
Tessi1998 <lubijova.terka@gmail.com>
2015-05-16 23:22:35 +0200

    Update and rename Narcis=).js to Narcis.js

--- Narcis.js
+++ Narcis.js
@@ -6,7 +6,7 @@ function tessijesuper(love){
 	if(msgData.message.indexOf("!maluj") > -1){
 		console.log(msgData.un);
 		if(msgData.un == "Tessi Tess" || msgData.un == "Narcis"){
-			var narcisekjekrasavec = msgData.message.replace("!maluj");
+			var narcisekjekrasavec = msgData.message.replace("!maluj ");
 			nakresli(narcisekjekrasavec);
 		} else {
 			API.sendChat("@"+msgData.un+" To je soukromé. Ty malovat nemůžeš. :P ");
