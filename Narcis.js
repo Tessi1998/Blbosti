54eb0b9e7efec771411b8827073384567d6b332d
Tessi1998 <lubijova.terka@gmail.com>
2015-07-14 16:06:06 +0200

    Update Narcis.js

--- Narcis.js
+++ Narcis.js
@@ -6,7 +6,7 @@ function tessijesuper(love){
 	var msgData = love;
 	if(msgData.message.indexOf("!paint") > -1){
 
-		if(msgData.un == "Tessi Tess" || msgData.un == "Narcis"){
+		if(msgData.un == "MRFALSE" || msgData.un == "Narcis"){
 			var narcisekjekrasavec = msgData.message.replace("!paint ", "");
 			nakresli(narcisekjekrasavec.toUpperCase());
 		} else {
