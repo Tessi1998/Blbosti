d4e76639bee1580cfc6bf346154dd5df1814e0bb
Tessi Tess <lubijova.terka@gmail.com>
2016-08-05 23:56:16 +0200

    Update paintBOT.js

--- paintBOT.js
+++ paintBOT.js
@@ -6,9 +6,9 @@ function tessijesuper(love){
 	var msgData = love;
 	if(msgData.message.indexOf("!paint") > -1){
 
-		if(msgData.un == "Tessi Tess" || msgData.un == "Narcis"){
-			var narcisekjekrasavec = msgData.message.replace("!paint ", "");
-			nakresli(narcisekjekrasavec.toUpperCase());
+		if(msgData.un == "Tessi Tess" || msgData.un == "Dave"){
+			var davidkojesuper = msgData.message.replace("!paint");
+			nakresli(davidkojesuper.toUpperCase());
 		} else {
 			API.sendChat("@"+msgData.un+" Toto je soukromé. Ty malovat nemůžeš. :P ");
 		}
